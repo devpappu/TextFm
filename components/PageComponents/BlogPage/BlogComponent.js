9214f886f42d024aa07f61a7e50a1af24fda6899
dev pappu <pappusha629@gmail.com>
2022-05-26 16:09:49 +0600

    arron icon added

--- components/PageComponents/BlogPage/BlogComponent.js
+++ components/PageComponents/BlogPage/BlogComponent.js
@@ -2,7 +2,6 @@ import React from 'react';
 import style from './blog.module.css'
 import blogTitleTrim from '../../../utility/blogTitleTrim';
 import Link from "next/link";
-
 const BlogComponent = ({blogs}) => {
     return (
         <>
@@ -14,9 +13,16 @@ const BlogComponent = ({blogs}) => {
                         <p className="mb-3 text-black text-2xl font-medium hover:text-red-600 ">{item.post_title}</p>
                         <div className=''>
                           <p className='mt-4 text-lg'>{blogTitleTrim(item.des)}
-                           <span className='ml-3 pb-1 border-b border-red-600 hover:text-red-600'>
-                             Read More ➾
-                           </span>
+                            <Link
+                                href={{
+                                  pathname: "/blog/[slug]",
+                                  query: { slug: item.slug },
+                                }}
+                              >
+                            <a className='ml-3 pb-1 border-b border-red-600 hover:text-red-600'>
+                              Read More ➾
+                            </a>
+                          </Link>
                            </p>
                           <p className='mt-6 text-gray-700 text-md'>By <span className='text-lg font-semibold pl-1 pr-4'>Team Textfm</span> January 27, 2022</p>
                         </div>

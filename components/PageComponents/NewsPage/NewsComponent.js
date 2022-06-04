d0522d826a1c588a242e3d022f3ad2c56088bbbf
dev pappu <pappusha629@gmail.com>
2022-06-04 20:09:55 +0600

    design update

--- components/PageComponents/NewsPage/NewsComponent.js
+++ components/PageComponents/NewsPage/NewsComponent.js
@@ -7,26 +7,32 @@ const NewsComponent = ({news}) => {
           {
             news.map((item, index) =>{
                 return(
-                <div key={index} className="mb-6 bg-white rounded-lg shadow-lg px-5 py-4 box__shadow border border-gray-200  ">
+                <div key={index} className="mb-6 bg-white rounded-lg shadow-lg px-5 pt-4 pb-2 box__shadow border border-gray-200  ">
                     <div className='mt-2 lg:flex md:flex justify-center items-center gap-5'>
 
                           <div className='lg:w-4/12 md:w-5/12 w-full'>
-                              <Image  width="500px" height="250px" src="/images/gig4.jpg" 
+                              <Image  width="500px" height="280px" src="/images/gig4.jpg" 
                                   className={`${style.blog__image} blog__image rounded-lg`}  alt={item.post_title}  />
                           </div>
 
                           <div className='lg:w-8/12 md:w-7/12'>
                           
                            <div>
-                              <p className="mt-3 lg:text-2xl md:text-2xl font-medium ">{item.post_title}</p>
+                              <p className="mt-3 lg:text-2xl text-lg">{item.post_title}</p>
                             </div>
 
                             <div>
+
+
                               <p className='mt-2' >{item.des}</p>
 
-                              <p className='mt-3 text-gray-700 text-sm'>By Textfm 25 Sep 2022</p>
+                              <p className='mt-4 text-gray-700 text-sm'>12-07-2022 05:13 PM</p>
 
-                                <button className='mt-3 text-left text-black py-3'>Read More At Time Now</button>
+                                <button className='mt-3 text-left text-black py-3'>Read more at 
+                                  <span className='ml-2  font-medium border-b border-black pb-1 '>
+                                    Time Now
+                                  </span>
+                                </button>
 
                             </div>
 
--- components/layouts/header/header.jsx
+++ components/layouts/header/header.jsx
@@ -7,6 +7,7 @@ import { MdOutlineDarkMode } from "react-icons/md";
 import SearchBox from './HeaderComponents/SearchBox.js';
 import { useDispatch, useSelector } from "react-redux";
 import { SET_SUBSCRIBE_FORM } from "../../../redux/actions/SubscribeFormAction";
+import { FaSistrix } from "react-icons/fa";
 
 const Header = () => {
 
@@ -103,140 +104,149 @@ const Header = () => {
               }
 
     </div>
+      <div className=" fixed top-0 z-50 w-full block ">
+        <div className="lg:px-48 bg-white text-black px-6 shadow-md border-b w-full block md:py-5 py-2 border-gray-100 bg-top-bar relative ">
 
-      <div className="lg:px-48 bg-blue px-6 shadow-md border-b-0 py-5 border-gray-100 bg-top-bar relative ">
-
-        <div className="lg:block md:block hidden ">
-          <div className="flex justify-between items-center gap-5">
-          <div className="flex gap-16 items-center w-4/12 ">
-            <div className="text-3xl">
-              <Link href="/">
-                TextFm
-              </Link>
-            </div>
-        
-            <div className={style.menu_ul__li}>
-              <ul className={`${style.menu__item} flex -ml-4  gap-10`}>
-                {primaryMenu.map((item, index) => {
-                  return (
-                    <Link key={index} href={item.menu_link}>
-                      <a className={style.menu__item}>{item.menu_title}</a>
-                    </Link>
-                  );
-                })}
-              </ul>
-            </div>
-          </div>
-
-
-          <div className="flex flex-wrap items-center gap-5">
-              <div>
-                  {
-                    user.email? <Link
-                    href="/dashboard"
-                    className={` ${style.sign__btn} text-white primary__bg__color py-2 rounded-full px-8`}
-                  >
-                    Dashboard
-                  </Link> :  <div className="flex gap-8">
-                  {/* <div onClick={() => dispatch(SET_SUBSCRIBE_FORM())} className=" cursor-pointer text-3xl "><FaSistrix/></div> */}
-                  <div onClick={changeMode} className=" cursor-pointer text-3xl "><MdOutlineDarkMode/></div>
-                  </div>
-                  }
-            </div>
-          </div>
-          </div>
-        </div>
-
-
-        {/* phone header */}
-        
-        <div className="lg:hidden md:hidden">
-
-          <div className="flex items-center justify-between ">
-
-          <div>
-            <span className="text-white text-2xl">
-              <Link href="/">
-                {/* <a> <Image src="/images/upwork.png" width="130px" height="30px" className='logo site__logo' alt="website logo" /></a> */}
-                TextFm
-              </Link>
-            </span>
-          </div>
-
-            <div className="flex items-center gap-8 text-white">
-                  {/* <div onClick={() => dispatch(SET_SUBSCRIBE_FORM())} className=" cursor-pointer text-3xl "><FaSistrix/></div> */}
-                  <div onClick={changeMode} className=" cursor-pointer text-3xl "><MdOutlineDarkMode/></div>
-                 <i onClick={toglePhoneMenu} className=" cursor-pointer text-4xl las la-bars"></i>
-            </div>
-          </div>
-        </div>
-
-
-
-        {/* phone left side menu */}
-
-        {phoneMenu && 
+          <div className="lg:block md:block hidden ">
+            <div className="flex justify-between items-center gap-5">
+            <div className="flex gap-16 items-center w-4/12 ">
+              <div className="text-3xl text-black ">
+                <Link href="/">
+                  TextFm
+                </Link>
+              </div>
           
-            <div className={`popup__background phone__header ${phoneMenu ? '': ''}`}>
-
-              <div className={`h-screen bg-white pb-4  absolute top-0 left-0  w-4/6 `}>
-              
-                <div className=''>
-                
-                  <div className="bg-blue w-full py-2 px-6 flex justify-between items-center ">
-                    <p className="text-white text-lg">Main Menu</p>
-
-                    <button onClick={toglePhoneMenu} className="bg-white px-4 rounded-lg text-red-500 text-4xl">x</button>
-
-                  </div>
-
-                    <ul className={`${style.menu__item} mt-4   text-black`}>
-                      {primaryMenu.map((item, index) => {
-                        return (
-                          <Link key={index} href={item.menu_link}>
-                            <a className={`block border-b border-gray-200 px-6 py-3 `}>{item.menu_title}</a>
-                          </Link>
-                        );
-                      })}
-                    </ul>
-
-                    <div className="px-6 mt-2 ">
+              <div className={style.menu_ul__li}>
+                <ul className={`${style.menu__item} flex -ml-4  gap-10`}>
+                  {primaryMenu.map((item, index) => {
+                    return (
+                      <Link key={index} href={item.menu_link}>
+                        <a className={style.menu__item}>{item.menu_title}</a>
+                      </Link>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
 
 
-                      {/* <p>Logout</p> */}
-            {
-              user.email? <Link
-              href="/dashboard"
-              className={` ${style.sign__btn} primary__bg__color py-2 rounded-full px-8 text-white `}
-            >
-              Dashboard a
-            </Link> :  <>
-            <Link
-              href="/login"
-              className={` ${style.sign__btn} text-white primary__bg__color py-2 rounded-full px-8 text-white`}
-              style={{textTransform: 'uppercase'}}
-            >
-              <a className={`${style.phone_menu_item}`}> Login</a>
-            </Link>
-            <Link
-              href="/signup"
-              className={` ${style.sign__btn} text-white primary__bg__color py-2 rounded-full px-8 text-white`}
-            >
-              <a className={`${style.phone_menu_item}`}>Sign Up</a>
-            </Link>
-
-            </>
-            }
-
+            <div className="flex flex-wrap items-center gap-5">
+                <div>
+                    {
+                      user.email? <Link
+                      href="/dashboard"
+                      className={` ${style.sign__btn} text-white primary__bg__color py-2 rounded-full px-8`}
+                    >
+                      Dashboard
+                    </Link> :  <div className="flex gap-8">
+                    {/* <div onClick={() => dispatch(SET_SUBSCRIBE_FORM())} className=" cursor-pointer text-3xl "><FaSistrix/></div> */}
+                    <div onClick={changeMode} className=" cursor-pointer text-3xl "><MdOutlineDarkMode/></div>
                     </div>
-                    
-                  </div>
+                    }
+              </div>
+            </div>
+            </div>
+          </div>
 
+
+          {/* phone header */}
+          
+          <div className="lg:hidden md:hidden">
+
+            <div className="flex items-center justify-between gap-20">
+
+            <div className="flex items-center  gap-8 text-black">
+                    {/* <div onClick={() => dispatch(SET_SUBSCRIBE_FORM())} className=" cursor-pointer text-3xl "><FaSistrix/></div> */}
+                    {/* <div onClick={changeMode} className=" cursor-pointer text-3xl "><MdOutlineDarkMode/></div> */}
+                  <i onClick={toglePhoneMenu} className=" cursor-pointer text-3xl las la-bars"></i>
               </div>
 
+            <div>
+              <span className="text-black font-bold text-2xl">
+                <Link href="/">
+                  {/* <a> <Image src="/images/upwork.png" width="130px" height="30px" className='logo site__logo' alt="website logo" /></a> */}
+                  TextFM
+                </Link>
+              </span>
             </div>
-        }
 
+            <div>
+              <span className="text-black font-bold text-2xl">
+                <FaSistrix/>
+              </span>
+            </div>
+
+            
+            </div>
+          </div>
+
+
+
+          {/* phone left side menu */}
+
+          {phoneMenu && 
+            
+              <div className={`popup__background phone__header ${phoneMenu ? '': ''}`}>
+
+                <div className={`h-screen bg-white pb-4  absolute top-0 left-0  w-4/6 `}>
+                
+                  <div className=''>
+                  
+                    <div className="bg-blue w-full py-2 px-6 flex justify-between items-center ">
+                      <p className="text-black text-lg">Main Menu</p>
+
+                      <button onClick={toglePhoneMenu} className="bg-white px-4 rounded-lg text-red-500 text-4xl">x</button>
+
+                    </div>
+
+                      <ul className={`${style.menu__item} mt-4   text-black`}>
+                        {primaryMenu.map((item, index) => {
+                          return (
+                            <Link key={index} href={item.menu_link}>
+                              <a className={`block border-b border-gray-200 px-6 py-3 `}>{item.menu_title}</a>
+                            </Link>
+                          );
+                        })}
+                      </ul>
+
+                      <div className="px-6 mt-2 ">
+
+
+                        {/* <p>Logout</p> */}
+              {
+                user.email? <Link
+                href="/dashboard"
+                className={` ${style.sign__btn} primary__bg__color py-2 rounded-full px-8 text-black `}
+              >
+                Dashboard a
+              </Link> :  <>
+              <Link
+                href="/login"
+                className={` ${style.sign__btn} text-black primary__bg__color py-2 rounded-full px-8`}
+                style={{textTransform: 'uppercase'}}
+              >
+                <a className={`${style.phone_menu_item}`}> Login</a>
+              </Link>
+              <Link
+                href="/signup"
+                className={` ${style.sign__btn} text-black primary__bg__color py-2 rounded-full px-8`}
+              >
+                <a className={`${style.phone_menu_item}`}>Sign Up</a>
+              </Link>
+
+              </>
+              }
+
+                      </div>
+                      
+                    </div>
+
+                </div>
+
+              </div>
+          }
+
+        </div>
       </div>
     </>
     

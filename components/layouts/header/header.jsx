48e11423935da5d6e3fb6973bbbdf020ba3d1208
dev pappu <pappusha629@gmail.com>
2022-06-06 15:01:22 +0600

    menu update

--- components/layouts/header/header.jsx
+++ components/layouts/header/header.jsx
@@ -143,48 +143,52 @@ const Header = () => {
               
                 <div className={`${style.phome__menu__div}`}>
                     <ul className={`${style.menu__item} mt-4   text-black`}>
-                      {primaryMenu.map((item, index) => {
-                        return (
-                          <div  key={index} className="px-6 flex gap-4 py-3">
+                    
+                     <div  className="px-6 flex gap-4 py-3">
+                      <i className="phone_menu_icon las la-globe-africa"></i>
+                      <Link href='/news'>
+                        <a className={`${style.phone__menu__item}`}>
+                           Top stories
+                          </a>
+                      </Link>
+                      </div>
 
-                            <span className="flex gap-3"><svg width="24" height="24" viewBox="0 0 24 24" focusable="false" className="hGhvff NMm5M"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM4 12c0-.61.08-1.21.21-1.78L8.99 15v1c0 1.1.9 2 2 2v1.93C7.06 19.43 4 16.07 4 12zm13.89 5.4c-.26-.81-1-1.4-1.9-1.4h-1v-3c0-.55-.45-1-1-1h-6v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41C17.92 5.77 20 8.65 20 12c0 2.08-.81 3.98-2.11 5.4z"></path></svg>
-                                                        
-                            </span>
+                     <div  className="phone_menu_icon px-6 flex items-center gap-4 py-3">
+                     <i className="lar la-hand-point-right"></i>
+                      <Link href='/news'>
+                        <a className={`${style.phone__menu__item}`}>
+                           For you
+                          </a>
+                      </Link>
+                      </div>
 
-                            <Link href={item.menu_link}>
-                              <a className={`${style.phone__menu__item}`}>
-                                {item.menu_title}</a>
-                            </Link>
+                     <div  className="phone_menu_icon items-center px-6 flex gap-4 py-3">
+                     <i className="las la-search"></i>
+                      <Link href='/news'>
+                        <a className={`${style.phone__menu__item}`}>
+                         Saved searches
+                          </a>
+                      </Link>
+                      </div>
 
-                          </div>
-                        );
-                      })}
+
+                     <div  className="px-6 flex items-center gap-4 py-3">
+                       <i className="phone_menu_icon las la-star-half-alt"></i>
+                      <Link href='/news'>
+                        <a className={`${style.phone__menu__item}`}>
+                           Following
+                          </a>
+                      </Link>
+                      </div>
+
                     </ul>
 
                    <div className="px-6">
-                    <div className="py-5 my-4 border-y flex items-center gap-3">
-                         <i className="phone_menu_icon las la-globe-africa"></i>
+                    <div className="py-5 my-4 phone_menu_icon border-y flex items-center gap-3">
+                         <i className="las la-notes-medical"></i>
                         <a className={`${style.phone__menu__item}`}>CAVID-19</a>
                       </div>
                    </div>
-                    <ul className={`${style.menu__item} mt-4   text-black`}>
-                      {primaryMenu.map((item, index) => {
-                        return (
-                          <div  key={index} className="px-6 flex gap-4 py-3">
-
-                            <span className="flex gap-3"><svg width="24" height="24" viewBox="0 0 24 24" focusable="false" className="hGhvff NMm5M"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM4 12c0-.61.08-1.21.21-1.78L8.99 15v1c0 1.1.9 2 2 2v1.93C7.06 19.43 4 16.07 4 12zm13.89 5.4c-.26-.81-1-1.4-1.9-1.4h-1v-3c0-.55-.45-1-1-1h-6v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41C17.92 5.77 20 8.65 20 12c0 2.08-.81 3.98-2.11 5.4z"></path></svg>
-                                                        
-                            </span>
-
-                            <Link href={item.menu_link}>
-                              <a className={`${style.phone__menu__item}`}>
-                                {item.menu_title}</a>
-                            </Link>
-
-                          </div>
-                        );
-                      })}
-                    </ul>
                     <ul className={`${style.menu__item} mt-4   text-black`}>
                       {primaryMenu.map((item, index) => {
                         return (

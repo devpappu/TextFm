f2e941027823ec141a32502d546293a8b24663e4
Dev Pappu <pappusha629@gmail.com>
2022-08-20 19:55:46 +0600

    page update

--- components/PageComponents/BlogPage/BlogComponent.js
+++ components/PageComponents/BlogPage/BlogComponent.js
@@ -72,41 +72,40 @@ const BlogComponent = ({blogs}) => {
               {
                 blogs.map((item, index) =>{
                     return(
-                    <div key={index} className="bg-white shadow border rounded px-4 py-3  flex gap-4">
-                           
-                            <div className=''>
+                      <>
+                        <Link href={`/blog/${item.slug}`}>
+                                <a>  
 
-                                  <p className="lg:block md:block hidden my-3 text-black text-lg font-medium hover:text-red-600 ">{item.post_title}</p>
+                                <div key={index} className="bg-white shadow border rounded px-4 py-3  flex gap-4">
+                         
+                               
+                         <div className=''>
+                            <p className=" my-3 text-black text-md font-medium hover:text-red-600 ">{blogTitleTrim(item.post_title, 60)}</p>
 
-                                  <p className="lg:hidden md:hidden block my-3 text-black text-md font-medium hover:text-red-600 ">{blogTitleTrim(item.post_title, 60)}</p>
+            
+                            
+                            <div className='flex justify-between items-center mt-1'>
+                               <p className=' text-gray-700 text-sm'> January 27, 2022</p>
 
-
-                                  <p className='lg:block md:block hidden mt-4 text-md'>{blogTitleTrim(item.des, 110)}
-                                    <Link
-                                        href={{
-                                          pathname: "/blog/[slug]",
-                                          query: { slug: item.slug },
-                                        }}
-                                      >
-                                    <a className='ml-3 pb-1 border-b border-red-600 hover:text-red-600'>
-                                      Read More ➾
-                                    </a>
-                                  </Link>
-                                  </p>
-                                  
-                                  <div className='flex justify-between items-center mt-1'>
-                                     <p className=' text-gray-700 text-sm'> January 27, 2022</p>
-
-                                     <svg width="25" height="25" viewBox="0 0 25 25" fill="none" className="mo"><path d="M18 2.5a.5.5 0 0 1 1 0V5h2.5a.5.5 0 0 1 0 1H19v2.5a.5.5 0 1 1-1 0V6h-2.5a.5.5 0 0 1 0-1H18V2.5zM7 7a1 1 0 0 1 1-1h3.5a.5.5 0 0 0 0-1H8a2 2 0 0 0-2 2v14a.5.5 0 0 0 .8.4l5.7-4.4 5.7 4.4a.5.5 0 0 0 .8-.4v-8.5a.5.5 0 0 0-1 0v7.48l-5.2-4a.5.5 0 0 0-.6 0l-5.2 4V7z" fill="#292929"></path></svg>
-                                  </div>
-                               </div>
-
-                            <div className='w-[250px]' >
-                                 <Image className='rounded object-cover ' width={'300px'} height={'350px'}
-                                   src={'/images/hrithik-roshan.jpg'}>
-                                 </Image>
+                               <svg width="25" height="25" viewBox="0 0 25 25" fill="none" className="mo"><path d="M18 2.5a.5.5 0 0 1 1 0V5h2.5a.5.5 0 0 1 0 1H19v2.5a.5.5 0 1 1-1 0V6h-2.5a.5.5 0 0 1 0-1H18V2.5zM7 7a1 1 0 0 1 1-1h3.5a.5.5 0 0 0 0-1H8a2 2 0 0 0-2 2v14a.5.5 0 0 0 .8.4l5.7-4.4 5.7 4.4a.5.5 0 0 0 .8-.4v-8.5a.5.5 0 0 0-1 0v7.48l-5.2-4a.5.5 0 0 0-.6 0l-5.2 4V7z" fill="#292929"></path></svg>
                             </div>
-                    </div>
+                            
+                         </div>
+
+                      <div className='w-[250px]' >
+                           <Image className='rounded object-cover ' width={'300px'} height={'350px'}
+                             src={'/images/hrithik-roshan.jpg'}>
+                           </Image>
+                      </div>
+
+                    
+              </div>
+
+
+                                </a>
+                        </Link>
+                      </>
+                    
                     )
                 })
               }
